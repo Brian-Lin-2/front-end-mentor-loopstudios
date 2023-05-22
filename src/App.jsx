d55b41dd786f9b3fe14c6e5fa92b0a097745b6d8
Brian <linbrian24@gmail.com>
2023-05-22 11:20:39 -0400

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import Header from './Header';
+import Info from './Info';
+import Creation from './Creation';
+import Footer from './Footer';
+
 export default function App() {
-  
+  return (
+    <div>
+      <Header />
+      <Info />
+      <Creation />
+      <Footer />
+    </div>
+  )
 }
