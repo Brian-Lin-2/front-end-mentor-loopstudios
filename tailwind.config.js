08d4cc98bacbc4f71c86476c269e5283c8fdfb8b
Brian <linbrian24@gmail.com>
2023-05-23 18:22:09 -0400

    Fix linear gradient error

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,12 +21,12 @@ export default {
         '26': '104px',
       },
       backgroundImage: {
+        'hero': "url('/images/mobile/image-hero.jpg')",
         'curiosity': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-curiosity.jpg')",
         'deep-earth': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-deep-earth.jpg')",
         'fisheye': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-fisheye.jpg')",
         'from-above': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-from-above.jpg')",
         'grid': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-grid.jpg')",
-        'hero': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-hero.jpg')",
         'night-arcade': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-night-arcade.jpg')",
         'pocket-borealis': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-pocket-borealis.jpg')",
         'soccer-team': "linear-gradient(to right, hsla(0, 0%, 0%, 50%), hsla(0, 0%, 100%, 0%)), url('/images/mobile/image-soccer-team.jpg')",
