f5a4e9d11d5330cd6e4313fbde946c03e477d246
Brian <linbrian24@gmail.com>
2023-05-24 12:34:20 -0400

    Style creation component (desktop)

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,11 @@
 export default function Card({ name_1, name_2, image }) {
-  const style = 'w-3/4 h-26 text-white bg-cover bg-no-repeat mb-5 ' + image;
+  const style = 'h-26 text-white bg-cover bg-no-repeat lg:h-96 ' + image;
 
   return (
     <>
       <div className={style}>
-        <p className="w-5/12 font-josefin-sans text-xl border-white relative top-12 left-4 leading-5">{name_1}</p>
-        <p className="w-5/12 font-josefin-sans text-xl border-white relative top-12 left-4 leading-5">{name_2}</p>
+        <p className="font-josefin-sans text-xl border-white relative top-12 left-4 leading-5 lg:top-80 lg:left-6 lg:text-2xl lg:leading-6">{name_1}</p>
+        <p className="font-josefin-sans text-xl border-white relative top-12 left-4 leading-5 lg:top-80 lg:left-6 lg:text-2xl lg:leading-6">{name_2}</p>
       </div>
     </>
   )
--- src/Creation.jsx
+++ src/Creation.jsx
@@ -14,14 +14,16 @@ export default function Creation() {
 
   return (
     <div className="tracking-wide flex flex-col items-center mb-32">
-      <div className="lg:flex lg:justify-between border lg:w-3/4">
-        <h1 className="font-josefin-sans text-3xl mb-12">OUR CREATIONS</h1>
-        <button className="border-2 border-black py-2 px-8 font-alata tracking-super-wide text-xs mt-2 hidden lg:block">SEE ALL</button>
+      <div className="lg:flex lg:justify-between lg:w-3/4 lg:items-center lg:mb-16">
+        <h1 className="font-josefin-sans text-3xl mb-12 lg:m-0 lg:text-4xl">OUR CREATIONS</h1>
+        <button className="border-2 border-black py-2 px-8 font-alata tracking-super-wide text-xs hidden lg:block">SEE ALL</button>
       </div>
 
-      {creations.map(creation => {
-          return <Card key={crypto.randomUUID()} name_1={creation.name_1} name_2={creation.name_2} image={creation.image} />
-      })}
+      <div className="grid grid-rows-8 grid-cols-1 w-3/4 justify-center lg:grid lg:grid-cols-4 lg:grid-rows-2 lg:justify-center gap-6">
+        {creations.map(creation => {
+            return <Card key={crypto.randomUUID()} name_1={creation.name_1} name_2={creation.name_2} image={creation.image} />
+        })}
+      </div>
 
       <button className="border-2 border-black py-2 px-8 font-alata tracking-super-wide text-xs mt-2 mb-2 lg:hidden">SEE ALL</button>
     </div>
