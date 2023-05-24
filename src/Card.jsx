db67b96b00c79537a49f6fe64a71ce79026346a3
Brian <linbrian24@gmail.com>
2023-05-24 18:51:49 -0400

    Start active state

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
 export default function Card({ name_1, name_2, image }) {
-  const style = 'h-26 text-white bg-cover bg-no-repeat lg:h-96 ' + image;
+  const style = 'h-26 bg-cover bg-no-repeat lg:h-96 lg:hover:opacity-50 ' + image;
 
   return (
     <>
-      <div className={style}>
-        <p className="font-josefin-sans text-xl border-white relative top-12 left-4 leading-5 lg:top-80 lg:left-6 lg:text-2xl lg:leading-6">{name_1}</p>
-        <p className="font-josefin-sans text-xl border-white relative top-12 left-4 leading-5 lg:top-80 lg:left-6 lg:text-2xl lg:leading-6">{name_2}</p>
+      <div className="h-26 lg:h-96 text-white lg:hover:cursor-pointer lg:hover:text-black">
+        <div className={style}></div>
+        <p className="font-josefin-sans text-xl border-white relative -top-12 left-4 leading-5 lg:-top-20 lg:left-6 lg:text-2xl lg:leading-6">{name_1}</p>
+        <p className="font-josefin-sans text-xl border-white relative -top-12 left-4 leading-5 lg:-top-20 lg:left-6 lg:text-2xl lg:leading-6">{name_2}</p>
       </div>
     </>
   )
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,11 +3,30 @@ export default function Menu() {
     <a>
       <img className="w-5 lg:hidden" src="/images/icon-hamburger.svg" alt="" />
       <div className="hidden lg:flex lg:text-white lg:gap-6 lg:text-15px">
-        <p>About</p>
-        <p>Careers</p>
-        <p>Events</p>
-        <p>Products</p>
-        <p>Support</p>
+        <div className="flex flex-col items-center">
+          <p className="hover:cursor-pointer">About</p>
+          <div className="border-t-2 w-1/3 mt-1"></div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="hover:cursor-pointer">Careers</p>
+          <div className="border-t-2 w-1/3 mt-1 hidden"></div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="hover:cursor-pointer">Events</p>
+          <div className="border-t-2 w-1/3 mt-1 hidden"></div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="hover:cursor-pointer">Products</p>
+          <div className="border-t-2 w-1/3 mt-1 hidden"></div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <p className="hover:cursor-pointer">Support</p>
+          <div className="border-t-2 w-1/3 mt-1 hidden"></div>
+        </div>
       </div>
     </a>
   )
