4aeee90b59b21060cb76fdda889ca6a2ca82436b
Brian <linbrian24@gmail.com>
2023-05-24 11:39:32 -0400

    Style info component (desktop)

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <Menu />
       </div>
 
-      <h2 className="text-white border-2 mx-8 px-7 py-5 text-4xl font-josefin-sans tracking-wide lg:w-1/2 lg:mx-40 lg:text-7xl lg:px-10 lg:py-8">
+      <h2 className="text-white border-2 mx-8 px-7 py-5 text-4xl font-josefin-sans tracking-wide lg:max-w-screen-sm lg:mx-40 lg:text-7xl lg:px-10 lg:py-8">
         IMMERSIVE EXPERIENCES THAT DELIVER
       </h2>
     </div>
