03a5f4f762528b8278f8cf812cf2277e676f5758
Brian <linbrian24@gmail.com>
2023-05-24 20:24:43 -0400

    Add mobile menu interactivity

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,9 @@ export default function Header() {
   return (
     <div className="bg-hero-mobile bg-cover bg-no-repeat pb-60 lg:h-desktop lg:bg-hero-desktop">
       <div className="flex justify-between items-center mx-10 pt-12 mb-40 lg:pt-16 lg:mx-44 lg:mb-32">
-
-        <img className="w-32" src="/images/logo.svg" alt="loopstudios"/>
+        <a className="hover:cursor-pointer">
+          <img className="w-32" src="/images/logo.svg" alt="loopstudios"/>
+        </a>
         
         <Menu />
 
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,19 +1,32 @@
 import MenuItems from './MenuItems';
+import { useState } from 'react';
 
 export default function Menu() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <>
-      <img className="w-5 lg:hidden hover:cursor-pointer" src="/images/icon-hamburger.svg" alt="" />
-      <div className="top-0 right-0 bottom-0 left-0 fixed z-10 bg-black text-white">
-        <div className="flex justify-between items-center mx-6 my-12">
-          <img className="w-40" src="/images/logo.svg" alt="loopstudios"/>
-          <img className="hover:mouse-pointer" src="images/icon-close.svg" />
-        </div>
+      <a className="hover:cursor-pointer" onClick={() => setOpenMenu(true)}>
+        <img className="w-5 lg:hidden" src="/images/icon-hamburger.svg" alt="" />
+      </a>
+      
+      { openMenu && 
+        <div className="top-0 right-0 bottom-0 left-0 fixed z-10 bg-black text-white">
+          <div className="flex justify-between items-center mx-6 my-12">
+            <a className="hover:cursor-pointer">
+              <img className="w-40" src="/images/logo.svg" alt="loopstudios"/>
+            </a>
+            
+            <a className="hover:cursor-pointer" onClick={() => setOpenMenu(false)}>
+              <img src="images/icon-close.svg" alt="" />
+            </a>
+          </div> 
         
-        <div className="w-5/12 flex flex-col items-start gap-6 mx-6 mt-48 text-2xl font-josefin-sans uppercase">
-          <MenuItems />
+          <div className="w-5/12 flex flex-col items-start gap-6 mx-6 mt-48 text-2xl font-josefin-sans uppercase">
+            <MenuItems />
+          </div>
         </div>
-      </div>
+      }
     </>
   );
 }
